2777974927ef49e1cd1cd2d3ecc27abc8ce2cd9d
Sukriti Rai <54813085+SukritiRai0@users.noreply.github.com>
2024-02-01 00:43:54 -0600

    Place holder values inserted for filetree

---
+++ myapp/src/FileTree.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const FileTree = ({ data }) => {
+  return (
+    <ul>
+      {data.map((item, index) => (
+        <li key={index}>
+          {item.isDirectory ? (
+            <span>📁 {item.name}</span>
+          ) : (
+            <span>📄 {item.name}</span>
+          )}
+          {item.parts && <FileTree data={item.parts} />}
+          {item.revisions && <FileTree data={item.revisions} />}
+          {item.trials && <FileTree data={item.trials} />}
+          {item.files && (
+            <ul>
+              {item.files.map((file, fileIndex) => (
+                <li key={fileIndex}>
+                  <span>📄 {file.name}</span>
+                </li>
+              ))}
+            </ul>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default FileTree;
